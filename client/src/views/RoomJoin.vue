435f769ff36391d7b2381f6b23f49590bc32a535
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-06-16 14:09:02 +0200

    Made room join prettier

--- client/src/views/RoomJoin.vue
+++ client/src/views/RoomJoin.vue
@@ -24,28 +24,76 @@ const roomInfo = computed(() => {
     throw Error("Cant handle online right now");
   }
 });
-function handleJoin() {
+function handleJoin(role: GameRole) {
   router.push({
     name: "play",
     params: {
       offline: null,
       roomID: roomInfo.value.roomID,
-      role: chosenRole.value,
+      role,
     },
   });
 }
 </script>
 
 <template>
-  <div>
-    {{ roomInfo.words.slice(0, 3).join("") }}
-    <br />
-    Join as
-    <select v-model="chosenRole">
-      <option value="leader" v-if="roomInfo.readAccess">Spymaster</option>
-      <option value="revealer" v-if="roomInfo.writeAccess">Revealer</option>
-      <option value="spectator">Spectator</option>
-    </select>
-    <div @click="handleJoin">Join</div>
+  <div class="box">
+    <span class="titleBox">{{ roomInfo.words.slice(0, 3).join("") }}</span>
+    <div class="selectionBox">
+      <div class="selectionBoxTitle">Join as</div>
+      <div class="selectionOptionBox">
+        <div
+          class="selectionBoxOption"
+          @click="handleJoin('leader')"
+          v-if="roomInfo.readAccess"
+        >
+          Spymaster
+        </div>
+        <div
+          class="selectionBoxOption"
+          @click="handleJoin('revealer')"
+          v-if="roomInfo.writeAccess"
+        >
+          Revealer
+        </div>
+        <div class="selectionBoxOption" @click="handleJoin('spectator')">
+          Spectator
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.selectionOptionBox {
+  display: flex;
+  flex-direction: row;
+}
+.selectionBoxOption {
+  border: 1px solid black;
+  margin: 0.2em;
+  padding: 0.2em;
+  transition: font-size ease 0.5s;
+}
+.selectionBoxOption:hover {
+  /* transform: scale(1.5); */
+  font-size: 200%;
+  background-color: white;
+}
+.selectionBoxTitle {
+  text-align: center;
+  font-weight: bold;
+}
+.titleBox {
+  border-bottom: 0.1em double black;
+  padding-left: 1em;
+  padding-right: 1em;
+  font-size: 200%;
+  margin-bottom: 0.3em;
+}
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
