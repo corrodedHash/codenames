2ef930d2f239b46746a3c4f0e523009bafc295e0
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-06-17 01:10:58 +0200

    Worked on server

--- server/main.py
+++ server/main.py
@@ -1,18 +1,10 @@
 import enum
+from typing import Annotated
 from uuid import UUID, uuid4
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator
-
-
-class Room(BaseModel):
-    words: list[str]
-    colors: list[str]
-    revealed: list[bool]
-    clickState: int
-    roomState: int
-    adminTokens: list[str]
-    participantTokens: list[str]
+from fastapi import Depends, FastAPI, HTTPException, Header, status
+from pydantic import BaseModel, validator, Field
+import datetime
 
 
 class CellColor(str, enum.Enum):
@@ -20,6 +12,17 @@ class CellColor(str, enum.Enum):
     BLUE = "blue"
     BLACK = "black"
     NEUTRAL = "neutral"
+
+
+class Room(BaseModel):
+    words: list[str]
+    colors: list[CellColor]
+    revealed: list[bool] = Field(default_factory=lambda: [False] * 25)
+    clickState: int = 0
+    roomState: int = 0
+    adminTokens: list[str] = Field(default_factory=list)
+    participantTokens: list[str] = Field(default_factory=list)
+    creation: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 
 class RoomCreationParams(BaseModel):
@@ -48,28 +51,74 @@ class RoomUpdateStatus(BaseModel):
     roomState: int
 
 
+class RoomSummary(BaseModel):
+    id: UUID
+    name: str
+
+
+class RoomCreationResponse(BaseModel):
+    id: UUID
+    token: str
+
+
 ROOMS: dict[UUID, Room] = {}
 
 app = FastAPI()
 
 
 @app.get("/rooms")
-def list_rooms():
-    pass
+def list_rooms() -> list[RoomSummary]:
+    return [
+        RoomSummary(id=k, name="".join([x.replace(" ", "") for x in v.words[:3]]))
+        for k, v in ROOMS.items()
+    ]
 
 
-@app.post("/room")
-def create_room(params: RoomCreationParams):
+@app.post("/room", status_code=status.HTTP_201_CREATED)
+def create_room(params: RoomCreationParams) -> RoomCreationResponse:
     if len(ROOMS) > 100:
-        raise HTTPException(status_code=507, detail="Too many rooms")
-    token = uuid4()
-    if token in ROOMS:
+        raise HTTPException(status_code=500, detail="Too many rooms")
+    roomID = uuid4()
+    if roomID in ROOMS:
         raise HTTPException(status_code=500, detail="Bad luck")
+    admintoken = uuid4().hex
+    ROOMS[roomID] = Room(
+        words=params.words, colors=params.colors, adminTokens=[admintoken]
+    )
+    return RoomCreationResponse(id=roomID, token=admintoken)
+
+
+class RoomCredentials:
+    def __init__(self, roomID: UUID, authorization: Annotated[str, Header()]):
+        room = ROOMS[roomID]
+        token = authorization.replace("Bearer ", "")
+        self.isAdmin = token in room.adminTokens
+        if not (self.isAdmin and token in room.participantTokens):
+            raise HTTPException(status_code=401)
 
 
 @app.patch("/room/{roomID}")
-def change_room(roomID: UUID, params: RoomCreationParams):
-    pass
+def change_room(
+    roomID: UUID,
+    params: RoomCreationParams,
+    creds: Annotated[RoomCredentials, Depends()],
+) -> None:
+    if not creds.isAdmin:
+        raise HTTPException(status_code=401)
+    room = ROOMS[roomID]
+    room.words = params.words
+    room.colors = params.colors
+    room.roomState += 1
+
+
+@app.delete("/room/{roomID}")
+def delete_room(
+    roomID: UUID,
+    creds: Annotated[RoomCredentials, Depends()],
+) -> None:
+    if not creds.isAdmin:
+        raise HTTPException(status_code=401)
+    del ROOMS[roomID]
 
 
 @app.get("/room/{roomID}")
