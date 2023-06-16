9b7b19592011194e64a8dc665d8539a34e48b774
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-06-16 20:14:08 +0200

    Worked on server

--- server/main.py
+++ server/main.py
@@ -1,13 +1,14 @@
 import enum
-from uuid import UUID,uuid4
+from uuid import UUID, uuid4
 
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel,validator
+from pydantic import BaseModel, validator
 
 
 class Room(BaseModel):
     words: list[str]
     colors: list[str]
+    revealed: list[bool]
     clickState: int
     roomState: int
     adminTokens: list[str]
@@ -25,10 +26,21 @@ class RoomCreationParams(BaseModel):
     words: list[str]
     colors: list[CellColor]
 
-    @validator('words')
-    def words_long_enough(cls, v: list[str]):
+    @validator("words")
+    def words_long_enough(cls, v: list[str]) -> list[str]:
         length_matches = len(v) == 25
-        wordlength_matches 
+        wordlength_matches = all(len(x) < 25 for x in v)
+        if not length_matches:
+            raise ValueError("Word list wrong size")
+        if not wordlength_matches:
+            raise ValueError("Words in word list too long")
+        return v
+
+    @validator("colors")
+    def colors_long_enough(cls, v: list[CellColor]) -> list[CellColor]:
+        if len(v) != 25:
+            raise ValueError("Color list wrong size")
+        return v
 
 
 class RoomUpdateStatus(BaseModel):
@@ -51,7 +63,8 @@ def create_room(params: RoomCreationParams):
     if len(ROOMS) > 100:
         raise HTTPException(status_code=507, detail="Too many rooms")
     token = uuid4()
-
+    if token in ROOMS:
+        raise HTTPException(status_code=500, detail="Bad luck")
 
 
 @app.patch("/room/{roomID}")
