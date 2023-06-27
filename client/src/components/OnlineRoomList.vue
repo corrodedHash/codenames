606ee09f7bdc1cad51a05d2841eaa8506b887db8
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-06-27 17:45:23 +0200

    Restructured code

--- client/src/components/OnlineRoomList.vue
+++ client/src/components/OnlineRoomList.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
 import { useRoomStore } from "../store";
-import { ref } from "vue";
-import { watch } from "vue";
-import { getRoomRole } from "../api";
 import { onMounted } from "vue";
-import { RoomRole, leqRoomRoles } from "../util/roomInfo";
 const roomStore = useRoomStore();
 const router = useRouter();
 
@@ -22,18 +18,6 @@ function handleJoin(sessionkey: string) {
 function handleDelete(roomID: string) {
   delete roomStore.rooms[roomID];
 }
-
-const shareToggle = ref(undefined as undefined | string);
-const shareOptions = ref(undefined as undefined | RoomRole[]);
-watch(shareToggle, (s) => {
-  if (s === undefined) {
-    shareOptions.value = undefined;
-    return;
-  }
-  getRoomRole(s, roomStore.rooms[s].sessiontoken).then((v: RoomRole) => {
-    shareOptions.value = leqRoomRoles(v);
-  });
-});
 </script>
 <template>
   <div
